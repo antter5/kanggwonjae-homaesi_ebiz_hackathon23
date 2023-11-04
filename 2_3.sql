3570246d2d044eb56281effa959b3c8f43c9e2f4
antter5 <57975560+antter5@users.noreply.github.com>
2023-11-04 18:07:15 +0900

    Update 2_3.sql

--- 2_3.sql
+++ 2_3.sql
@@ -4,9 +4,9 @@ JOIN Orders ON Customers.customer_id = Orders.customer_id
 JOIN Shippings ON Customers.customer_id = Shippings.customer
 WHERE Orders.item = 'Keyboard';
 /*
-"first_name", "country", "status" 칼럼을 출력합니다.
-"Customers" 테이블에서 데이터를 가져옵니다.
-"Orders" 테이블을 "Customers" 테이블과 결합하고, "customer_id"를 기준으로 합니다.
-"Shippings" 테이블을 "Customers" 테이블과 결합하고, "customer_id"를 기준으로 합니다.
-"Orders" 테이블의 "item"이 'Keyboard'인 행만 선택합니다.
+"first_name", "country", "status" 칼럼을 출력한다.
+"Customers" 테이블에서 데이터를 가져온다.
+"Orders" 테이블을 "Customers" 테이블과 결합하고, "customer_id"를 기준으로 한다.
+"Shippings" 테이블을 "Customers" 테이블과 결합하고, "customer_id"를 기준으로 한다.
+"Orders" 테이블의 "item"이 'Keyboard'인 행만 선택한다.
 */
